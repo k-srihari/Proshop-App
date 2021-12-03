781e24b4a69ac2b6e281d68907ecd4d697ac6de5
k-srihari <sriharik998@gmail.com>
2021-12-03 17:17:38 +0530

    Header and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import './bootstrap.css'
+import { StrictMode } from "react";
+
 function App() {
   return (
-    <>
-    <h2>Welcome to the Pro Shop</h2>
-    </>
+    <StrictMode>
+    <Header />
+    <main>
+      <h2 className="py-3 text-center">Welcome to the Pro Shop</h2>
+    </main>
+    <Footer />
+    </StrictMode>
   );
 }
 
